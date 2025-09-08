a2995462d2fbbf04528b485b252a1f3b98f63549
Amal Paul <iamamalpaul1367@gmail.com>
2025-09-08 12:23:06 +0530

    db link changes

--- configuration/db.js
+++ configuration/db.js
@@ -2,23 +2,20 @@ const mongoose = require('mongoose');
 
 const connectToDB = async () => {
   try {
-    const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sportshub';
 
-    // Connect
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/JTM';
+
+    await mongoose.connect(mongoURI);
 
     console.log('MongoDB connected successfully');
 
-    await mongoose.connection.db.collection('users').indexes();
-    await mongoose.model('users').syncIndexes();
+    const User = mongoose.model('users');
+    await User.syncIndexes();
     console.log('User indexes synced with schema');
-    
+
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
-    process.exit(1); 
+    process.exit(1);
   }
 };
 
