9ed5c810cedc729a69fd6932e4b8283a2c9bbc9c
Amal Paul <iamamalpaul1367@gmail.com>
2025-09-12 17:33:32 +0530

    aboutLogin page added

--- routes/aboutLogin.js
+++ routes/aboutLogin.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const controller = require("../controllers/aboutLogin");
+const { checkSession, isLoggin } = require('../middlewares/auth');
+const userAuthantication = require('../middlewares/userAuthantication');
 
-router.get('/', controller.getAboutLogin);
+router.get('/', userAuthantication, checkSession, controller.getAboutLogin);
 
 module.exports = router;
